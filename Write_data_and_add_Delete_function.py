568d21cb9c72810dafb99c794cc1cf223f2604e2
This-is-Becky <165574421+This-is-Becky@users.noreply.github.com>
2025-09-04 14:32:03 +0800

    Update Write_data_and_add_Delete_function.py

--- Write_data_and_add_Delete_function.py
+++ Write_data_and_add_Delete_function.py
@@ -60,7 +60,7 @@ def main():
                 if len(parts) > 1:
                     data = parts[1].split(',')
                     writer.writerow([timestamp] + data)
-                    csvfile.flush()
+                    file.flush()
 
             # Rotate file every minute
             if datetime.now().strftime('%Y%m%d%H%M') != file_time.strftime('%Y%m%d%H%M'):
@@ -77,4 +77,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
 
